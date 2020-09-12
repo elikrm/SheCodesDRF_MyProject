1897b621b0343fd263420205e83aa252d5ac8d7e
elikrm <elnazkarimpoor85@gmail.com>
2020-09-12 12:36:47 +1000

    UsersAPI_Permissions_Authentication

---
+++ crowdfunding/users/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class CustomUserSerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    username = serializers.CharField(max_length=200)
+    email = serializers.CharField(max_length=200)
+
+    def create(self, validated_data):
+        return CustomUser.objects.create(**validated_data)
+
+# class CustomUserDetailSerializer(serializers.Serializer):
+
+#     def update(self, instance, validated_data):
+#         instance.username = validated_data.get('username', instance.username)
+#         instance.email = validated_data.get('email',instance.email)
+
+#         instance.save()
+#         return instance
